00bc0fcbd11be3f15a0230ad8dad8940262aacee
Sezb99 <sezacarias@gmail.com>
2024-07-23 12:00:54 -0400

    Se agrego a funcionalidad del login, con creacion y envio de token por medio de una cookie

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -26,9 +26,37 @@ export const register = async (req, res) => {
 
     // res.send(userSave);
   } catch (error) {
-    res.status(450).send(error);
+    res.status(500).json({ msg: error.message });
     console.log(error);
   }
 };
 
-export const login = (req, res) => res.send("login");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const userFound = await User.findOne({ email });
+
+    if (!userFound) return res.status(400).json({ msg: "invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+
+    if (!isMatch) return res.status(400).json({ msg: "invalid credentials" });
+
+    const token = await createAccesToken({ id: userFound._id });
+
+    res.cookie("token", token);
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt,
+    });
+
+    // res.send(userSave);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+    console.log(error);
+  }
+};
